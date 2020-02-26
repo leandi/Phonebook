1e9ce1b71cf9cfee30faa9fee643cb7e9da52e80
leandi <sandi.kurtin@gmail.com>
2020-02-26 11:57:01 +0100

    dodana slika

--- src/main/java/com/leandi/phonebook/ui/MainLayout.java
+++ src/main/java/com/leandi/phonebook/ui/MainLayout.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -24,12 +25,15 @@ public class MainLayout extends AppLayout {
     private void createHeader() {
         H1 logo = new H1("Telefonski imenik MOK");
         logo.addClassName("logo");
+        //Image image = new Image("https://dummyimage.com/100x100/000/fff", "DummyImage");
+        Image image = new Image("/img/logomok.png", "MOK Grb");
 
-
-        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), image, logo);
         header.addClassName("header");
         header.setWidth("100%");
+        //header.add(image);
         header.expand(logo);
+
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
 
         addToNavbar(header);
