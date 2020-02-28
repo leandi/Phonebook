8d0df80853e60f80dc6ef76236f5a834cab624c9
leandi <sandi.kurtin@gmail.com>
2020-02-28 13:31:57 +0100

    dodana forma za urade, v bistvu je to tabela podjetij, ki jo uporabljam za urade ali službe. 2.Narejena 2 faza kjer uporabljam formo za vnos uradov.

--- src/main/java/com/leandi/phonebook/ui/views/updateForm/CompanyForm.java
+++ src/main/java/com/leandi/phonebook/ui/views/updateForm/CompanyForm.java
@@ -1,24 +1,102 @@
 package com.leandi.phonebook.ui.views.updateForm;
 
 import com.leandi.phonebook.backend.entity.Company;
+
+import com.leandi.phonebook.ui.views.list.ContactForm;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.shared.Registration;
 
 public class CompanyForm extends FormLayout {
-/*
-    TextField compapanyName = new TextField("Urad ali služba");
+    TextField name = new TextField("Urad ali služba");
 
     Button save = new Button("Shrani");
     Button delete = new Button("Zbriši");
     Button close = new Button("Prekliči");
 
-    Binder<Company> binder = BeanValidationBinder<>(Company.class);
+    Binder<Company> binder = new BeanValidationBinder<>(Company.class);
 
-    public CompanyForm(TextField compapanyName) {
-        this.compapanyName = compapanyName;
+    public CompanyForm() {
+        addClassName("contact-form");
 
-    }*/
+        binder.bindInstanceFields(this);
+
+        add(name, createButtonsLayout());
+
+    }
+
+    public void setCompany(Company company) {
+        binder.setBean(company);
+    }
+
+    private Component createButtonsLayout() {
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        save.addClickShortcut(Key.ENTER);
+        close.addClickShortcut(Key.ESCAPE);
+
+        save.addClickListener(click -> validateAndSave());
+        delete.addClickListener(click -> fireEvent(new CompanyForm.DeleteEvent(this, binder.getBean())));
+        close.addClickListener(click -> fireEvent(new CloseEvent(this)));
+
+        binder.addStatusChangeListener(evt -> save.setEnabled(binder.isValid()));
+        return new HorizontalLayout(save, delete, close);
+    }
+
+    private void validateAndSave() {
+        if (binder.isValid()) {
+            fireEvent(new SaveEvent(this, binder.getBean()));
+        }
+    }
+
+
+// Events
+public static abstract class CompanyFormEvent extends ComponentEvent<CompanyForm> {
+    private Company company;
+
+    protected CompanyFormEvent(CompanyForm source, Company company) {
+        super(source, false);
+        this.company = company;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+}
+
+    public static class SaveEvent extends CompanyFormEvent {
+        SaveEvent(CompanyForm source, Company company) {
+            super(source, company);
+        }
+    }
+
+    public static class DeleteEvent extends CompanyFormEvent {
+        DeleteEvent(CompanyForm source, Company company) {
+            super(source, company);
+        }
+
+    }
+
+    public static class CloseEvent extends CompanyFormEvent {
+        CloseEvent(CompanyForm source) {
+            super(source, null);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                  ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
 }
--- src/main/java/com/leandi/phonebook/ui/views/updateForm/ListCompany.java
+++ src/main/java/com/leandi/phonebook/ui/views/updateForm/ListCompany.java
@@ -5,6 +5,7 @@ import com.leandi.phonebook.backend.entity.Company;
 import com.leandi.phonebook.backend.service.CompanyService;
 import com.leandi.phonebook.ui.MainLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.gridpro.GridPro;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -15,6 +16,7 @@ import com.vaadin.flow.router.Route;
 @PageTitle("Vnos Uradov ali služb")
 public class ListCompany extends VerticalLayout {
 
+    private final CompanyForm formCompany;
     Grid<Company> gridCompany = new Grid<>(Company.class);
 
     private CompanyService companyService;
@@ -25,7 +27,8 @@ public class ListCompany extends VerticalLayout {
         setSizeFull();
         configureGrid();
 
-        Div content = new Div(gridCompany);
+        formCompany = new CompanyForm();
+        Div content = new Div(gridCompany, formCompany);
         content.addClassName("content");
         content.setSizeFull();
 
@@ -39,10 +42,24 @@ public class ListCompany extends VerticalLayout {
         gridCompany.setSizeFull();
         gridCompany.setColumns("name");
         gridCompany.getColumns().forEach(col -> col.setAutoWidth(true));
+        gridCompany.asSingleSelect().addValueChangeListener(evt -> editCompany(evt.getValue()));
     }
 
+    private void editCompany(Company company) {
+        if (company == null) {
+            closeEditor();
+        }  else {
+            formCompany.setCompany(company);
+            formCompany.setVisible(true);
+            addClassName("editing");
+        }
+    }
+    private void closeEditor() {
+        formCompany.setCompany(null);
+        formCompany.setVisible(false);
+        removeClassName("editing");
+    }
     private void updateList() {
-
         gridCompany.setItems(companyService.findAll());
     }
 
