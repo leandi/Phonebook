03150cec4b63279915f92e7ebd7d13e3ac2a236d
leandi <sandi.kurtin@gmail.com>
2020-03-13 11:06:09 +0100

    to je pregled forme pred logiranjem

--- src/main/java/com/leandi/phonebook/ui/views/login/LoginView.java
+++ src/main/java/com/leandi/phonebook/ui/views/login/LoginView.java
@@ -1,9 +1,15 @@
 package com.leandi.phonebook.ui.views.login;
 
+import com.leandi.phonebook.backend.entity.Company;
+import com.leandi.phonebook.backend.entity.Contact;
+import com.leandi.phonebook.backend.service.CompanyService;
+import com.leandi.phonebook.backend.service.ContactService;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -16,23 +22,53 @@ import java.util.Collections;
 public class LoginView extends HorizontalLayout implements BeforeEnterObserver {
 
     LoginForm login = new LoginForm();
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    private ContactService contactService;
+    TextField filterText = new TextField();
 
-    public LoginView() {
-        addClassName("login-view");
+    public LoginView(ContactService contactService,
+                     CompanyService companyService) {
+
+
+        this.contactService = contactService;
+
+        addClassName("list-view");
+        //addClassName("login-view");
         setSizeFull();
-
+        configureGrid();
+        updateList();
         //setJustifyContentMode(JustifyContentMode.CENTER);
        //setAlignItems(Alignment.CENTER);
 
         login.setAction("login");
 
         add(
-            new H1("Telefonski imenik MOK"),
-            login
+            //new H1("Telefonski imenik MOK"),
+            grid, login
         );
     }
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "phoneNumber");
+//        grid.setColumns();
+//        grid.addColumn(Contact::getFirstName).setHeader(new H1("imr"));
+//        grid.addColumn(Contact::getLastName).setHeader("Priimek");
 
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setSortable(true).setHeader("company");
+// set sortable doda možnost sortiranja
 
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        //grid.asSingleSelect().addValueChangeListener(evt -> editContact(evt.getValue()));
+    }
+    private void updateList() {
+        grid.setItems(contactService.findAll(filterText.getValue()));
+    }
     @Override
     public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
         if(!beforeEnterEvent.getLocation()
