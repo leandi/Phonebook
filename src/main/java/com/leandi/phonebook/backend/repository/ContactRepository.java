d8dcb56f386211759943bd30c5ba6e9026948d4a
leandi <sandi.kurtin@gmail.com>
2020-04-14 18:13:20 +0200

    dodal sem iskanje po telefonskih številkah

--- src/main/java/com/leandi/phonebook/backend/repository/ContactRepository.java
+++ src/main/java/com/leandi/phonebook/backend/repository/ContactRepository.java
@@ -9,7 +9,24 @@ import java.util.List;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     @Query("select c from Contact c " +
-          "where lower(c.firstName) like lower(concat('%', :searchTerm, '%')) " +
-          "or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))")
+            "where lower(c.firstName) like lower(concat('%', :searchTerm, '%')) " +
+            "or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))" +
+            "or c.phoneNumber like concat('%', :searchTerm, '%')" +
+            "or c.gsmNumber like concat('%', :searchTerm, '%')")
     List<Contact> search(@Param("searchTerm") String searchTerm);
 }
+
+
+
+/*
+@Query("select c from Contact c " +
+        "where lower(c.firstName) like lower(concat('%', :searchTerm, '%')) " +
+        "or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))")
+
+ @Query("select c from Contact c " +
+          "where lower(c.firstName) like lower(concat('%', :searchTerm, '%')) " +
+          "or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))" +
+          "or c.phoneNumber like ('%', :searchTerm, '%')" +
+            "or c.gsmNumber like ('%', :searchTerm, '%')")
+
+*/
